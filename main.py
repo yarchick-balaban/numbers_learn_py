fd0ecc0647a3d735062d01499c809cfb5d3cd6a3
Yarchick333 <yarchick.balaban@gmail.com>
2021-03-27 16:47:17 +0200

    Add numbers generator file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from random import randint
+
+MIN_NUMBER = -10000
+MAX_NUMBER = 10000
+AMOUNT_NUMBER = 5000000
+
+
+if __name__ == "__main__":
+    number_list = []
+
+    for i in range(AMOUNT_NUMBER):
+        random_number = randint(MIN_NUMBER, MAX_NUMBER)
+        number_list.append(random_number)
 
